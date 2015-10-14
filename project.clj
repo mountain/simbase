f2114812411a8a92fbeb20843177f627e895577f
Mingli Yuan <mingli.yuan@gmail.com>
2015-10-14 18:35:27 +0800

    upgrade to new version of each dependencies

--- project.clj
+++ project.clj
@@ -8,15 +8,15 @@
             :distribution :repo
             :comments "same as Clojure"}
 
-    :dependencies [[org.clojure/clojure "1.5.1"]
-                   [org.yaml/snakeyaml "1.12"]
-                   [clj-pid "0.1.1"]
+    :dependencies [[org.clojure/clojure "1.7.0"]
+                   [org.yaml/snakeyaml "1.16"]
+                   [clj-pid "0.1.2"]
                    [net.sf.trove4j/trove4j "3.0.3"]
-                   [org.slf4j/slf4j-api "1.7.5"]
-                   [org.slf4j/slf4j-log4j12 "1.7.5"]
+                   [org.slf4j/slf4j-api "1.7.12"]
+                   [org.slf4j/slf4j-log4j12 "1.7.12"]
                    [log4j/log4j "1.2.17"]
-                   [com.esotericsoftware.kryo/kryo "2.22"]
-                   [junit "4.11" :scope "test"]]
+                   [com.esotericsoftware.kryo/kryo "2.24.0"]
+                   [junit "4.12" :scope "test"]]
     :source-paths ["src/main/clojure"]
     :java-source-paths ["src/main/java"]
     :resource-paths ["src/main/resources"]
@@ -27,7 +27,7 @@
                      :all (constantly true)}
     :compile-path "target/classes"
     :target-path "target/"
-    :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+    :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
     :jvm-opts ["-Xmx1g"]
 
     :uberjar-name "simbase-standalone.jar"
